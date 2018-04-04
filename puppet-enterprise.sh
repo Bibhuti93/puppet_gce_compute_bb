2245e90dedabb7004f6a1010ffe85327328b7641
Bibhuti93 <bibhuti.mymail@gmail.com>
2018-04-04 10:50:02 +0530

    Create puppet-enterprise.sh

---
+++ puppet-enterprise.sh
@@ -0,0 +1,3 @@
+mkdir /opt/puppet-enterprise
+curl -L -o /opt/puppet-enterprise.tgz 'https://pm.puppet.com/cgi-bin/download.cgi?dist=ubuntu&rel=16.04&arch=amd64&ver=latest'
+tar -xvf /opt/puppet-enterprise.tgz
